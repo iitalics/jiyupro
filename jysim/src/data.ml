4940703a8b6d0e8b6133c205083c6d145fe0a3ea
Milo Turner <iitalics@gmail.com>
2017-05-16 21:34:01 +0000

    better type aliases

--- jysim/src/data.ml
+++ jysim/src/data.ml
@@ -1,20 +1,24 @@
-(* TODO: change these to int types with more bits *)
-type hashcode = int
-type timestamp = int
-type art_id = int
-type sign = int
+type uniq_id = int64   (* dunno *)
+type timestamp = int64 (* 8-byte unix timestamp *)
+type hashcode = bytes  (* 256 bits = 32 bytes *)
+type signature = bytes (* signature is a signed hashcode *)
+type pubkey = bytes    (* N-bit key based on what algorithm?? *)
+type chan_id = string  (* arbitrary identifier (max-length: 32 chars) *)
 
-type art_type = Root
+type art_data =
+  | ArtRoot    of pubkey
+  | ArtInvite  of (pubkey * signature * int)
+  | ArtThread  of chan_id
+  | ArtReply   of (uniq_id * bytes)
 
-type artifact = { art_id : art_id
+type artifact = { art_id : uniq_id
                 ; art_ts : timestamp
-                ; art_author : art_id
-                ; art_sign : sign
-                ; art_type : art_type
-                ; art_contents : string }
+                ; art_author : uniq_id
+                ; art_sign : signature
+                ; art_contents : art_data }
 
 type block = { bl_prev : hashcode
              ; bl_ts : timestamp
-             ; bl_author : art_id
-             ; bl_sign : sign
+             ; bl_author : uniq_id
+             ; bl_sign : signature
              ; bl_arts : artifact list }
--- jysim/src/main.ml
+++ jysim/src/main.ml
@@ -2,10 +2,4 @@ open Printf
 open Data
 
 let () =
-  let b = { bl_prev = 0
-          ; bl_ts = 0
-          ; bl_author = 0
-          ; bl_sign = 0
-          ; bl_arts = [] }
-  in
   printf "Hello, world!\n"
